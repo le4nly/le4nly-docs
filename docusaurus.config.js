c54b8aa9bd8e3303b5ce84cb10e3997f8f31565e
Tobi <tobias.gremmer@gmail.com>
2023-10-02 08:34:12 +0200

    Move to custom domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://le4nly.github.io',
+  url: 'https://docs.le4nly.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/le4nly-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
